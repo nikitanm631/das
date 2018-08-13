dac6f2181b140757150e78de34e4de94f1c14f7b
nikitanm631 <nikita@cyberrafting.com>
2018-08-13 16:55:32 +0530

    single service started

---
+++ daycare.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Daycare Facility </title>
+  	<meta name = "keywords" content = "cardiologist">
+  	<meta name = "description" content = "cardiologist in vashi navi mumbai">
+    <?php include 'includes/head.php' ?>
+  </head>
+  <body>
+
+    <?php include 'includes/menu.php' ?>
+
+    <!-- SINGLE HEADER -->
+    <section class="banner-head">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10">
+            <ol class="breadcrumb">
+              <li><a href="index.php">Home</a></li>
+              <li><a href="services.php">Services</a></li>
+              <li class="active">Daycare</li>
+            </ol>
+            <h2>Daycare</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END SINGLE HEADER -->
+
+    <section class="main-content pt-55 pb-40">
+      <div class="container-fluid">
+        <div class="row">
+
+          <div class="col-md-8">
+
+          </div>
+
+          <div class="col-md-4">
+            <?php include 'includes/sidebar.php' ?>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+
+    <?php include 'includes/footer.php' ?>
+
+  </body>
+</html>
---
+++ includes/sidebar.php
@@ -0,0 +1,42 @@
+<div class="sidelist">
+  <ul>
+
+    <a href="#"> <li class="active">D&C (Dilation and Curettage)</li> </a>
+    <a href="#"> <li>Laparoscopic Adjustable Gastric Band</li> </a>
+    <a href="#"> <li>Gastric Balloon</li> </a>
+    <a href="#"> <li>Gastric Plication</li> </a>
+    <a href="#"> <li>Mini Port Surgeries</li> </a>
+    <a href="#"> <li>Laparoscopic Sleeve Resection</li> </a>
+    <a href="#"> <li>Single Port Surgeries</li> </a>
+    <a href="#"> <li>Gall Bladder (Biliary) Stone Treatment</li> </a>
+    <a href="#"> <li>Colonoscopy</li> </a>
+    <a href="#"> <li>Endoscopic Surgery</li> </a>
+    <a href="#"> <li>Laparoscopic Surgery</li> </a>
+    <a href="#"> <li>Laparoscopic Hernial Repair</li> </a>
+    <a href="#"> <li>Laparoscopic Hiatus Hernia Repair</li> </a>
+    <a href="#"> <li>Laparoscopic Nephrectomy</li> </a>
+    <a href="#"> <li>Laparoscopic Cholecystectomy</li> </a>
+    <a href="#"> <li>Laparoscopic Surgery</li> </a>
+    <a href="#"> <li>Endoscopy - Diagnostic & Therapeutic</li> </a>
+    <a href="#"> <li>ICCU</li> </a>
+    <a href="#"> <li>Dialysis Unit & Nephrology Department</li> </a>
+    <a href="#"> <li>Well Equipped Rooms</li> </a>
+    <a href="#"> <li>2D Echo / ColourDoppler / Ultra Sonography</li> </a>
+    <a href="#"> <li>C Arm in OT</li> </a>
+    <a href="#"> <li>All types of Surgeries</li> </a>
+    <a href="#"> <li>X Ray</li> </a>
+    <a href="#"> <li>Blood Test</li> </a>
+    <a href="#"> <li>Pathology</li> </a>
+    <a href="#"> <li>Ambulance</li> </a>
+    <a href="#"> <li>First Aid</li> </a>
+    <a href="#"> <li>Daycare</li> </a>
+    <a href="#"> <li>Surgery Theatre</li> </a>
+    <a href="#"> <li>Medication</li> </a>
+    <a href="#"> <li>General Medical Consultation</li> </a>
+    <a href="#"> <li>Health Checkup</li> </a>
+    <a href="#"> <li>Orthopaedics</li> </a>
+    <a href="#"> <li>Accident & Trauma Care</li> </a>
+    <a href="#"> <li>ICU</li> </a>
+    <a href="#"> <li>24 Hour Pathology</li> </a>
+  </ul>
+</div>
