0f75ee638a7b16ce2883c5f4029d9dbfe1722139
nikitanm631 <nikita@cyberrafting.com>
2018-08-13 12:01:06 +0530

    added

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Dr. Pooja Multani (Ghogare) </title>
+	<meta name = "keywords" content = "cardiologist">
+	<meta name = "description" content = "cardiologist in vashi navi mumbai">
+
+	<?php include 'includes/head.php'; ?>
+</head>
+<body>
+	<?php include 'includes/menu.php'; ?>
+	<div class="wrapper">
+
+		<!-- BANNER SECTION -->
+		<section class="banner-main">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		    <li data-target="#myCarousel" data-slide-to="1"></li>
+		    <li data-target="#myCarousel" data-slide-to="2"></li>
+		  </ol>
+
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner">
+		    <div class="item active">
+		      <img src="myimages/slider/1.jpg" alt="">
+		    </div>
+
+		    <div class="item">
+		      <img src="myimages/slider/2.jpg" alt="">
+		    </div>
+
+
+		  </div>
+
+		  <!-- Left and right controls -->
+		  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+		</section>
+		<!-- END BANNER SECTION -->
+
+	</div>
+	<?php include 'includes/footer.php'; ?>
+</body>
+</html>
