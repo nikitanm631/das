c0f9f2f3b9492f6c5c1d6589c9eac991ed16efb2
nikitanm631 <nikita@cyberrafting.com>
2018-08-13 14:57:34 +0530

    first changes

--- includes/menu.php
+++ includes/menu.php
@@ -1,89 +1,91 @@
-<!-- TOP BAR -->
-<section class="top-bar">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-5">
-        <img src="myimages/logo.jpg" alt="" class="img-responsive">
-      </div>
-      <div class="col-md-7">
-        <div class="row">
+<section class="shadow">
+  <!-- TOP BAR -->
+  <section class="top-bar">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5">
+          <img src="myimages/logo.jpg" alt="" class="img-responsive nav-img">
+        </div>
+        <div class="col-md-7">
+          <div class="row">
 
-          <div class="col-md-5">
-            <div class="inline">
-              <i class="fa fa-clock-o"></i>
-              <div class="info">
-                <p> Working Hours </p>
-                <p>Monday - Friday 8AM - 7PM </p>
+            <div class="col-md-4">
+              <div class="inline">
+                <img src="myimages/clock.png" alt="" class="img-responsive">
+                <div class="info">
+                  <p> Working Hours </p>
+                  <p>Monday - Friday 8AM - 7PM </p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div class="col-md-3">
-            <div class="inline">
-              <i class="fa fa-phone"></i>
-              <div class="info">
-                <p> Call Us At </p>
-                <p>123-456-7890 </p>
+            <div class="col-md-4">
+              <div class="inline">
+                <img src="myimages/phone.png" alt="" class="img-responsive">
+                <div class="info">
+                  <p> Call Us At </p>
+                  <p>123-456-7890 </p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div class="col-md-4">
-            <div class="inline">
-              <i class="fa fa-map-marker"></i>
-              <div class="info">
-                <p> Find Us </p>
-                <p>Address </p>
+            <div class="col-md-4">
+              <div class="inline">
+                <img src="myimages/marker.png" alt="" class="img-responsive">
+                <div class="info">
+                  <p> Find Us </p>
+                  <p>Address </p>
+                </div>
               </div>
             </div>
-          </div>
 
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
-<!-- END TOP BAR -->
+  </section>
+  <!-- END TOP BAR -->
 
 
 
-<!-- NAV BAR -->
-<section class="nav-sec">
-  <nav class="navbar navbar">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
+  <!-- NAV BAR -->
+  <section class="nav-sec">
+    <nav class="navbar navbar">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
 
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Departments</a></li>
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Services
+              <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Page 1-1</a></li>
+                <li><a href="#">Page 1-2</a></li>
+                <li><a href="#">Page 1-3</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"> <i class="fa fa-facebook"></i> </a></li>
+            <li><a href="#"> <i class="fa fa-twitter"></i> </a></li>
+            <li><a href="#"> <i class="fa fa-google-plus"></i> </a></li>
+          </ul>
+        </div>
       </div>
-      <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Departments</a></li>
-          <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Services
-            <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Page 1-1</a></li>
-              <li><a href="#">Page 1-2</a></li>
-              <li><a href="#">Page 1-3</a></li>
-            </ul>
-          </li>
-          <li><a href="#">Gallery</a></li>
-          <li><a href="#">Blog</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#"> <i class="fa fa-facebook"></i> </a></li>
-          <li><a href="#"> <i class="fa fa-twitter"></i> </a></li>
-          <li><a href="#"> <i class="fa fa-google-plus"></i> </a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+    </nav>
+  </section>
+  <!-- END NAV BAR -->
 </section>
-<!-- END NAV BAR -->
--- index.php
+++ index.php
@@ -14,38 +14,70 @@
 		<!-- BANNER SECTION -->
 		<section class="banner-main">
 			<div id="myCarousel" class="carousel slide" data-ride="carousel">
-		  <!-- Indicators -->
-		  <ol class="carousel-indicators">
-		    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		    <li data-target="#myCarousel" data-slide-to="1"></li>
-		    <li data-target="#myCarousel" data-slide-to="2"></li>
-		  </ol>
+			  <!-- Indicators -->
+			  <ol class="carousel-indicators">
+			    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			    <li data-target="#myCarousel" data-slide-to="1"></li>
+			    <li data-target="#myCarousel" data-slide-to="2"></li>
+			  </ol>
 
-		  <!-- Wrapper for slides -->
-		  <div class="carousel-inner">
-		    <div class="item active">
-		      <img src="myimages/slider/1.jpg" alt="">
-		    </div>
+			  <!-- Wrapper for slides -->
+			  <div class="carousel-inner">
+			    <div class="item active">
+			      <img src="myimages/slider/1.jpg" alt="">
+			    </div>
 
-		    <div class="item">
-		      <img src="myimages/slider/2.jpg" alt="">
-		    </div>
+			    <div class="item">
+			      <img src="myimages/slider/2.jpg" alt="">
+			    </div>
 
 
-		  </div>
+			  </div>
 
-		  <!-- Left and right controls -->
-		  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-		    <span class="glyphicon glyphicon-chevron-left"></span>
-		    <span class="sr-only">Previous</span>
-		  </a>
-		  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-		    <span class="glyphicon glyphicon-chevron-right"></span>
-		    <span class="sr-only">Next</span>
-		  </a>
-		</div>
+			  <!-- Left and right controls -->
+			  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div>
 		</section>
 		<!-- END BANNER SECTION -->
+
+		<!-- APPOINTMENT SECTION -->
+		<section class="appointment-sect">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1">
+						<div class="app-form bg-red">
+							<div class="row">
+								<div class="col-md-5">
+									<h2>Book an appointment</h2>
+								</div>
+								<div class="col-md-7">
+									<form class="form-inline" action="index.html" method="post">
+										<div class="form-group">
+											<input type="text" name="" value="" class="form-control">
+										</div>
+										<div class="form-group">
+											<input type="text" name="" value="" class="form-control">
+										</div>
+										<div class="form-group">
+											<input type="submit" name="submit" value="Send" class="btn btn-default">
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- END APPOINTMENT SECTION -->
+
 
 	</div>
 	<?php include 'includes/footer.php'; ?>
