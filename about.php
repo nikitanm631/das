8ceeeb5fb52a66c88d028fc0d81044e0d9a898d9
nikitanm631 <nikita@cyberrafting.com>
2018-08-16 15:32:17 +0530

    contact page

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Das Hospital | About Us </title>
+	<meta name = "keywords" content = "multi-speciality hospital">
+	<meta name = "description" content = "multi-speciality hospital in mumbai">
+
+	<?php include 'includes/head.php'; ?>
+</head>
+<body>
+	<?php include 'includes/menu.php'; ?>
+	<div class="wrapper">
+
+    <!-- SINGLE BANNER -->
+    <section class="single-banner">
+      <div class="container">
+        <h1>About Us</h1>
+
+      </div>
+    </section>
+    <!-- END SINGLE BANNER -->
+
+	</div>
+	<?php include 'includes/footer.php'; ?>
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Das Hospital | Contact Us </title>
+	<meta name = "keywords" content = "multi-speciality hospital">
+	<meta name = "description" content = "multi-speciality hospital in mumbai">
+
+	<?php include 'includes/head.php'; ?>
+</head>
+<body>
+	<?php include 'includes/menu.php'; ?>
+	<div class="wrapper">
+
+    <!-- SINGLE BANNER -->
+    <section class="single-banner">
+      <div class="container">
+        <h1>Contact Us</h1>
+
+      </div>
+    </section>
+    <!-- END SINGLE BANNER -->
+
+    <section class="main-sec pt-55 pb-40">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-md-6">
+            <h1 class="heading">Get in <span class="text-red">Touch</span> </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+            <br>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15084.372942394519!2d72.8997605!3d19.0596379!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6303c334d153505e!2sDr+Das+Hospital!5e0!3m2!1sen!2sin!4v1534413006858" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+
+
+          <div class="col-md-6">
+            <h1 class="heading">Send us <span class="text-red">a message</span> </h1>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+	</div>
+	<?php include 'includes/footer.php'; ?>
+</body>
+</html>
